bbf0dd9b0bb96549567f729889ad39b6a43e0bb6
Fang Tiancong <2830923997@qq.com>
2025-09-27 11:51:17 +0800

    fix: 将默认 git 的编辑器从 code  改成 “code --wait”

--- lib/set_git.sh
+++ lib/set_git.sh
@@ -6,7 +6,7 @@ function set_global_git_config() {
     git config --global user.email "2830923997@qq.com"
 
     # 设置默认编辑器
-    git config --global core.editor "code"
+    git config --global core.editor "code --wait"
 
     # 设置默认分支名
     git config --global init.defaultBranch "main"
@@ -30,22 +30,22 @@ Git 全局配置已完成！
 
 git config --global user.name "Fang Tiancong"
 git config --global user.email "2830923997@qq.com"
-    
+
 # 设置默认编辑器
-git config --global core.editor "code"
-    
+git config --global core.editor "code --wait"
+
 # 设置默认分支名
 git config --global init.defaultBranch "main"
-    
+
 # 设置换行符处理（Linux/Mac）
 # true : 提交时 CRLF -> LF;检出时 LF -> CRLF。推荐 Linux/Mac 用户使用
 # input: 提交时 CRLF -> LF;检出时不变。推荐 windows 用户使用
 # false: 不进行转换
 git config --global core.autocrlf input
-    
+
 # 设置文件权限变更检测
 git config --global core.filemode true
-    
+
 # 设置大小写敏感（推荐）
 git config --global core.ignorecase false
 
